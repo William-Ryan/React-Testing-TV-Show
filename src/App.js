957165a6ee2c18e31f224b3b0c404bb867aca878
William Ryan <billy.ryan.rom@gmail.com>
2020-02-25 09:35:28 -0500

    Add api dir and fetchshow.js/Add api call in fetchShow.js/Refractor useEffect to fetchshow import

--- src/App.js
+++ src/App.js
@@ -4,28 +4,22 @@ import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
+import { fetchShow } from "./api/fetchShow";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
 export default function App() {
   const [show, setShow] = useState(null);
-  const [seasons, setSeasons] = useState(null);
+  const [seasons, setSeasons] = useState({});
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow().then(showData => {
+      setShow(showData);
+      setSeasons(formatSeasons(showData._embedded.episodes));
+    });
   }, []);
 
   const handleSelect = e => {
